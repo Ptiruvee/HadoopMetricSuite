415e1464a0afdc8c51a792865f8e9785ac94a38e
adithya-g <adithyag@sase.ssn.edu.in>
2014-10-12 17:17:57 -0400

    Base class for invocation

---
+++ HadoopMetricSuite/src/com/hms/dummy/DummyBaseClass.java
@@ -0,0 +1,12 @@
+package com.hms.dummy;
+
+public class DummyBaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//This is where from which all of us can invoke our code and test
+		//Modify and use this class, but never commit!
+	}
+
+}
