5062b9a18b56b1bbd97158933ae4615b121b8845
adithya-g <adithyag@sase.ssn.edu.in>
2014-10-12 22:13:22 -0400

    ClusterMaster - connection, slave list (SB1, SB2)

--- HadoopMetricSuite/src/com/hms/common/Constants.java
+++ HadoopMetricSuite/src/com/hms/common/Constants.java
@@ -1,5 +1,18 @@
 package com.hms.common;
 
 public class Constants {
+	
+	//Error codes
+	public static final String NO_MASTER_CONNECTION = "100";
+	public static final String NO_SLAVE_LIST = "101";
+	
+	//User path
+	public static final String USER_PATH = "/home/ec2-user/";
+	
+	//Hadoop version & path
+	public static final String HADOOP_VERSION = "hadoop-2.5.0";
+	public static final String SLAVE_LIST_PATH = "/etc/hadoop/slaves";
 
+	//Boundary
+	public static final int MAXIMUM_SLAVES = 100;
 }
--- HadoopMetricSuite/src/com/hms/connection/ClusterMaster.java
+++ HadoopMetricSuite/src/com/hms/connection/ClusterMaster.java
@@ -1,5 +1,93 @@
 package com.hms.connection;
 
+import com.hms.common.Constants;
+
+import net.neoremind.sshxcute.core.ConnBean;
+import net.neoremind.sshxcute.core.Result;
+import net.neoremind.sshxcute.core.SSHExec;
+import net.neoremind.sshxcute.exception.TaskExecFailException;
+import net.neoremind.sshxcute.task.CustomTask;
+import net.neoremind.sshxcute.task.impl.ExecCommand;
+
 public class ClusterMaster {
 
+	//Private
+	SSHExec sshMaster = null;
+	
+	//Public
+	public String[] slaves = null;
+	public String errorCode = null;
+	
+	public boolean connectToMaster(String ipAddress, String username, String password)
+	{
+		if (!(ipAddress.length() > 0 && username.length() > 0 && password.length() > 0))
+		{
+			return false;
+		}
+		
+		try {
+
+			ConnBean cb = new ConnBean(ipAddress, username, password);
+
+			sshMaster = SSHExec.getInstance(cb);          
+
+			return sshMaster.connect();
+
+		} catch (Exception e) {
+
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+
+			return false;
+		} 
+	}
+	
+	public boolean fetchSlaveList()
+	{
+		if (sshMaster == null)
+		{
+			errorCode = Constants.NO_MASTER_CONNECTION;
+			
+			return false;
+		}
+		
+		try {
+			CustomTask catCmd = new ExecCommand("cat " + Constants.USER_PATH + Constants.HADOOP_VERSION + Constants.SLAVE_LIST_PATH);
+
+			Result catCmdRes = sshMaster.exec(catCmd);
+
+			if (catCmdRes.isSuccess)
+			{
+				System.out.println("Return code: " + catCmdRes.rc);
+				System.out.println("sysout: " + catCmdRes.sysout);
+				
+				slaves = catCmdRes.sysout.split("\n");
+				
+				return true;
+			}                        
+			else
+			{
+				System.out.println("Return code: " + catCmdRes.rc);
+				System.out.println("error message: " + catCmdRes.error_msg);
+			}
+		} catch (TaskExecFailException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} 
+		
+		errorCode = Constants.NO_SLAVE_LIST;
+
+		return false;
+	}
+	
+	public void disconnectMaster()
+	{
+		if (sshMaster != null)
+		{
+			sshMaster.disconnect();
+		}
+	}
 }
