3d0cfd93d7919e1377f6ade852b463ed30578bc8
adithya-g <adithyag@sase.ssn.edu.in>
2014-10-27 23:57:35 -0400

    Commiting base class since it will be required for next transistion

--- HadoopMetricSuite/src/com/hms/dummy/DummyBaseClass.java
+++ HadoopMetricSuite/src/com/hms/dummy/DummyBaseClass.java
@@ -1,5 +1,11 @@
 package com.hms.dummy;
 
+import com.hms.common.Constants;
+
+import com.hms.common.JobSession;
+import com.hms.common.UserLog;
+import com.hms.connection.ClusterMaster;
+
 public class DummyBaseClass {
 
 	public static void main(String[] args) {
@@ -7,6 +13,21 @@ public class DummyBaseClass {
 
 		//This is where from which all of us can invoke our code and test
 		//Modify and use this class, but never commit!
+		
+		JobSession.username = "ec2-user";
+		JobSession.password = "";
+		
+		ClusterMaster master = new ClusterMaster();
+		
+		if (master.connectToMaster("", JobSession.username, JobSession.password))
+		{
+			master.fetchSlaveList();
+			master.transferAndRunScriptFile();
+			master.runApplicationJob(Constants.WORD_COUNT);
+			master.disconnectMaster();
+		}
+		
+		System.out.println("Log message" + UserLog.getUserLog());
 	}
 
 }
