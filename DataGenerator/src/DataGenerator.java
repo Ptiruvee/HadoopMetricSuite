63d4f7c644707d203b5aaa73c0063d4ef476646e
sreevatson <sreevatsonmc@sase.ssn.edu.in>
2014-11-13 01:00:26 -0500

    Updated Data Generator

--- DataGenerator/src/DataGenerator.java
+++ DataGenerator/src/DataGenerator.java
@@ -9,9 +9,9 @@ import java.nio.file.StandardCopyOption;
 public class DataGenerator {
 	static int count;
 
-	public static void generateFile(int size) {
+	public static void generateFile(int gb,int mb) {
 		File file = new File("new.txt");
-		try {
+		/*try {
 			FileWriter filewriter = new FileWriter(file);
 			BufferedWriter writer = new BufferedWriter(filewriter);
 			PrintWriter printer = new PrintWriter(writer);
@@ -30,27 +30,45 @@ public class DataGenerator {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}*/
+		
+		File src = new File("OSWI.txt");
+		new File("data").mkdir();
+		int counter=0;
+		int gbcount=0, mbcount=0;
+		gbcount = (int) ((gb*1024) / 2.6);
+		mbcount = (int) (mb/2.6);
+		for(int i=0;i< mbcount;i++ ) {
+		File target ;
+
+		try {
+			target = new File("data/data"+counter+".txt");
+			Files.copy(src.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			counter++;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
+		}
 	}
 
 	public static void main(String[] args) {
 		long start,end;
-		start=System.currentTimeMillis();
-		generateFile(1024*1024*10);
-		end=System.currentTimeMillis();
-		System.out.println((double)(end-start)/1000);
-		File src = new File("OSWI.txt");
-		//new File("data").mkdir();
-		//int replication;
-		File target = new File("data/copy.txt");
-
-		/*try {
-			Files.copy(src.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		
+		//System.out.println((double)(end-start)/1000);
+		if(args.length != 1) {
+			System.out.println("Invalid argument length");
+			return;
+		}
+		double size;
+		int gb,mb;
+		if(args.length == 1) {
+			size = Double.parseDouble(args[0]);
+			gb = (int)size;
+			mb = (int) ((size - gb)*1024);
+			generateFile(gb,mb);
+		}
 
 	}
 }
