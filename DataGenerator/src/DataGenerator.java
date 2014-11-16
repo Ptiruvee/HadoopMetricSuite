496f2c9f897f675781ed8d52f909c4a2526a9c4b
sreevatson <sreevatsonmc@sase.ssn.edu.in>
2014-11-16 01:20:59 -0500

    Updated DataGenerator to generate in a single file

--- DataGenerator/src/DataGenerator.java
+++ DataGenerator/src/DataGenerator.java
@@ -1,86 +1,70 @@
-import java.io.BufferedWriter;
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 
 public class DataGenerator {
 	static int count;
 
-	public static void generateFile(int gb,int mb) {
-		File file = new File("new.txt");
-		/*try {
-			FileWriter filewriter = new FileWriter(file);
-			BufferedWriter writer = new BufferedWriter(filewriter);
-			PrintWriter printer = new PrintWriter(writer);
-			printer.append("hello");
-			int i = 0;
-			char c = 'a';
-			for (int j = 0; j < size; j++ ) {
-
-				printer.append(c);
-				//System.out.println(c+" "+j);
-				c++;
-				if (c == 'z')
-					c = 'a';
-			}
-			printer.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
-		File src = new File("OSWI.txt");
-		new File("data").mkdir();
-		int counter=0;
-		int gbcount=0, mbcount=0;
-		gbcount = (int) ((gb*1024) / 2.6);
-		mbcount = (int) (mb/2.6);
-		File target ;
-		for(int i=0;i< mbcount;i++ ) {
-		
-
+	public void generateFile(int gb, int mb) {
+		InputStream is = getClass().getResourceAsStream("OSWI.txt");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		StringBuilder sb = new StringBuilder();
+		String temp;
 		try {
-			target = new File("data/data"+counter+".txt");
-			Files.copy(src.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
-			counter++;
+			temp = reader.readLine();
+			while (temp != null) {
+				sb.append(temp);
+				sb.append("\n");
+				temp = reader.readLine();
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		int gbcount = 0, mbcount = 0;
+		gbcount = (int) ((gb * 1024) / 2.6);
+		mbcount = (int) (mb / 2.6);
+
+		PrintWriter out;
+		try {
+			out = new PrintWriter(new FileWriter("DataFile.txt", true));
+			for (int i = 0; i < mbcount; i++) {
+
+				out.append(sb.toString());
+				out.append("\n");
+
+			}
+			for (int i = 0; i < gbcount; i++) {
+
+				out.append(sb.toString());
+				out.append("\n");
+
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		}
-		for(int i=0;i< gbcount;i++ ) {
-
-			try {
-				target = new File("data/data"+counter+".txt");
-				Files.copy(src.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
-				counter++;
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			}
 	}
 
 	public static void main(String[] args) {
-		long start,end;
-		
-		//System.out.println((double)(end-start)/1000);
-		if(args.length != 1) {
+		// System.out.println((double)(end-start)/1000);
+		if (args.length != 1) {
 			System.out.println("Invalid argument length");
 			return;
 		}
+		DataGenerator generator = new DataGenerator();
 		double size;
-		int gb,mb;
-		if(args.length == 1) {
+		int gb, mb;
+		if (args.length == 1) {
 			size = Double.parseDouble(args[0]);
-			gb = (int)size;
-			mb = (int) ((size - gb)*1024);
-			generateFile(gb,mb);
+			gb = (int) size;
+			mb = (int) ((size - gb) * 1024);
+			generator.generateFile(gb, mb);
 		}
 
 	}
