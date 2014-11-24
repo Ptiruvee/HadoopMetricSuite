09502ceb9f09fa325d0e724aef53282f36d76017
adithya-g <adithyag@sase.ssn.edu.in>
2014-11-23 19:20:57 -0500

    Resource updates
    Database updated, script updated

--- HadoopMetricSuite/scripts/Platform.sh
+++ HadoopMetricSuite/scripts/Platform.sh
@@ -2,13 +2,13 @@
 
 while true ; do
 
-CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
+CPU=$(grep 'cpu ' /proc/stat | awk '{usage=100 * ($5-$4)/$5} END {print usage}')
 
-DISK=$(grep 'xvda ' /proc/diskstats | awk '{usage=($4)/($8)} END {print usage}')
+DISK=$(grep 'xvda ' /proc/diskstats | awk '{{read=$4} {write=$8} {readtime=$7} {writetime=$11} {iotime=$13}} END {print read, write, readtime, writetime, iotime}')
 
 MEM=$(cat /proc/meminfo | head -2 | awk 'NR == 1 { total = $2 } NR == 2 { free = $2 } END { print 1.0 - (free/total) }')
 
-NET=$(grep 'eth0' /proc/net/dev | sed -e 's/eth0://g' | awk '{print $1/$9}')
+NET=$(grep 'eth0' /proc/net/dev | sed -e 's/eth0://g' | awk '{print $2, $10}')
 
 TIME=$(date +%s)
 
